e08a4d5457ff28531d7d68e2397aeef04ae59664
Érick da silva <eierickdasilva@gmail.com>
2025-04-19 14:03:46 -0300

    feat: adding pages and components structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,20 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'menu',
+    loadComponent: () => import('./pages/menu/menu.page').then( m => m.MenuPage)
+  },
+  {
+    path: 'settings',
+    loadComponent: () => import('./pages/settings/settings.page').then( m => m.SettingsPage)
+  },
+  {
+    path: 'task',
+    loadComponent: () => import('./pages/task/task.page').then( m => m.TaskPage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
 ];
---
+++ src/app/components/nutritional-information-modal/nutritional-information-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nutritional-information-modal',
+  templateUrl: './nutritional-information-modal.component.html',
+  styleUrls: ['./nutritional-information-modal.component.scss'],
+})
+export class NutritionalInformationModalComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
