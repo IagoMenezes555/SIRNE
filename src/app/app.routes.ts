4563d3dbfee7e1dc362750f29a9ec94fb38567be
Érick da silva <eierickdasilva@gmail.com>
2025-04-19 14:14:07 -0300

    feat: adding navigate with footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,24 @@
 import { Routes } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
-  {
-    path: 'menu',
-    loadComponent: () => import('./pages/menu/menu.page').then( m => m.MenuPage)
+    component: FooterComponent,
+    children: [
+      {
+        path: 'menu',
+        loadComponent: () => import('./pages/menu/menu.page').then((m) => m.MenuPage),
+      },
+      {
+        path: 'task',
+        loadComponent: () => import('./pages/task/task.page').then((m) => m.TaskPage),
+      }
+    ]
   },
   {
     path: 'settings',
     loadComponent: () => import('./pages/settings/settings.page').then( m => m.SettingsPage)
-  },
-  {
-    path: 'task',
-    loadComponent: () => import('./pages/task/task.page').then( m => m.TaskPage)
   },
   {
     path: 'login',
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { IonTabs, IonTabBar ,IonTabButton, IonLabel } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
+  imports: [
+    IonTabs,
+    IonTabBar,
+    IonTabButton,
+    IonLabel
+  ]
 })
-export class FooterComponent  implements OnInit {
+export class FooterComponent {
 
   constructor() { }
-
-  ngOnInit() {}
-
 }
