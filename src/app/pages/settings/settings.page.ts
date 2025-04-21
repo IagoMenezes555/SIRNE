0346eb2e8f16de8a15068844b1dd659ab099edde
Érick da silva <eierickdasilva@gmail.com>
2025-04-21 18:51:43 -0300

    feat: adding settings page
    Settings page
    
    - toggle theme
    - credits

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,20 +1,90 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnDestroy } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, ModalController, IonButtons, Platform, IonToggle } from '@ionic/angular/standalone';
+import { caretBack, chevronBackOutline } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
+import { AuthService } from 'src/app/services/auth.service';
+import { SettingsService } from 'src/app/services/settings.service';
+import { Subscription } from 'rxjs';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { StatusBar, Style } from '@capacitor/status-bar';
+
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButton,
+    IonButtons,
+    FormsModule,
+    IonToggle
+  ]
 })
-export class SettingsPage implements OnInit {
+export class SettingsPage implements OnDestroy {
+  private backButtonSubscription: Subscription;
+  public isDarkMode: boolean = false;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    public settingsService: SettingsService,
+    private modalCtrl: ModalController,
+    private platform: Platform,
+  ) {
+    addIcons({ chevronBackOutline, caretBack });
 
-  ngOnInit() {
+    this.backButtonSubscription = this.platform.backButton.subscribeWithPriority(10000, async () => {
+      const modal = await this.modalCtrl.getTop();
+      if (modal) {
+        this.closeModal();
+      }
+    });
+
+    this.loadTheme();
   }
 
+  ngOnDestroy() {
+    this.backButtonSubscription.unsubscribe();
+  }
+
+  public loadTheme() {
+    const storedTheme = localStorage.getItem('dark-mode');
+
+    if (storedTheme) {
+      const theme = JSON.parse(storedTheme);
+      this.isDarkMode = theme;
+    } else {
+      this.isDarkMode = false
+    }
+  }
+
+  public toggleTheme() {
+    if (this.isDarkMode) {
+      document.documentElement.classList.remove('app-light');
+      document.documentElement.classList.add('app-dark');
+
+      StatusBar.setBackgroundColor({ color: '#7662DA' });
+      StatusBar.setStyle({ style: Style.Dark });      
+    } else {
+      document.documentElement.classList.remove('app-dark');
+      document.documentElement.classList.add('app-light');
+      
+      StatusBar.setBackgroundColor({ color: '#423671' });
+      StatusBar.setStyle({ style: Style.Dark });
+    }
+
+    localStorage.setItem('dark-mode', this.isDarkMode.toString());
+  }
+
+  public closeModal() {
+    this.modalCtrl.dismiss();
+  }
+
+  public logout() {
+    this.authService.logout();
+    this.closeModal();
+  }
 }
