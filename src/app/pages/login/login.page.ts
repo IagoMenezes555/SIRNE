8a10d220061491290308d777bc1b20bea437d273
Érick da silva <eierickdasilva@gmail.com>
2025-04-21 18:42:51 -0300

    feat: adding login screen

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,20 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonAvatar, IonItem, IonButton, ModalController } from '@ionic/angular/standalone';
+import { MenuService } from 'src/app/services/menu.service';
+import { ProfileModalComponent } from 'src/app/components/profile-modal/profile-modal.component';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonItem, IonAvatar, 
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule
+  ]
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  public name: string = '';
+  public appid: string = '';
+  public appkey: string = '';
 
-  constructor() { }
+  constructor(
+    private menuService: MenuService,
+    private auth: AuthService,
+    private modalCtrl: ModalController
+  ) { }
 
-  ngOnInit() {
+  public async openProfileModal() {
+    const modal = await this.modalCtrl.create({
+      component: ProfileModalComponent,
+    });
+    await modal.present();
   }
 
+  public login() {
+    this.auth.setLogin({ appid: this.appid, appkey: this.appkey, name: this.name });
+    this.menuService.loadBackendless();
+  }
 }
