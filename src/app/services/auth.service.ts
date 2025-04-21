65be4f3457ef9b6812d7d0245b055719876a4221
Érick da silva <eierickdasilva@gmail.com>
2025-04-21 18:41:45 -0300

    feat: adding auth service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,62 @@
+import { Injectable, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { Auth } from '../models/auth';
+import { AlertController } from '@ionic/angular/standalone';
+import { StorageService } from './storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private authenticated = signal<boolean>(this.hasToken());
+
+  constructor (
+    private storageService: StorageService,
+    private route: Router,
+    private alertController: AlertController,
+  ) {}
+
+  public isAuthenticated(): boolean {
+    return this.authenticated();
+  }
+
+  public setLogin(auth: Auth) {
+    this.storageService.save('auth', auth);
+
+    this.authenticated.set(true);
+    this.route.navigate(['menu']);
+  }
+
+  public async logout() {    
+    const alert = await this.alertController.create({
+      header: 'Confirmar Deleção',
+      message: "Tem certeza que deseja sair?",
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {},
+        },
+        {
+          text: 'Sair',
+          handler: () => {
+            this.authenticated.set(false);
+            this.route.navigate(['login']);
+          },
+        },
+      ],
+    });
+  
+    await alert.present();
+  }
+
+  private hasToken(): boolean {
+    const auth = localStorage.getItem('auth');
+
+    if (auth) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
