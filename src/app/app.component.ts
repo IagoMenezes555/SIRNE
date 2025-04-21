9ba7226df1b3242b46f02434f327a59f83b47654
Érick da silva <eierickdasilva@gmail.com>
2025-04-21 18:50:42 -0300

    feat: adding loading of theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { StatusBar, Style } from '@capacitor/status-bar';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,34 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+  public darkMode = false;
+  
+  constructor() {
+    this.initializeApp();
+  }
+
+  public initializeApp() {
+    this.initTheme();
+  }
+
+  private initTheme() {
+    const storedTheme = localStorage.getItem('dark-mode');
+
+    if (storedTheme) {
+      const theme = JSON.parse(storedTheme);
+
+      if (theme) {
+        document.documentElement.classList.add('app-dark');
+        StatusBar.setBackgroundColor({ color: '#ff6600' });
+        StatusBar.setStyle({ style: Style.Dark });
+      } else {
+        document.documentElement.classList.add('app-light');
+        StatusBar.setBackgroundColor({ color: '#ff6600' });
+        StatusBar.setStyle({ style: Style.Dark });
+      }
+    } else {
+      document.documentElement.classList.add('app-light');
+      localStorage.setItem('dark-mode', 'false');
+    }
+  }
 }
