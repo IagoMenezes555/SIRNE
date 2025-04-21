bd3089001b52167ecce745a7daa90f44489da524
Érick da silva <eierickdasilva@gmail.com>
2025-04-21 15:56:01 -0300

    feat: adding task service
    Task services:
    
    - add task
    - remove task
    - make id of task
    - save tasks
    - check task
    - load task

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,61 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { StorageService } from './storage.service';
+import { Task } from '../models/task';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  public tasks = signal<Task[]>([]);
 
-  constructor() { }
+  constructor(
+    private storageService: StorageService,
+  ) {
+    this.loadTasks();
+  }
+
+  public loadTasks() {
+    const tasksSaved = this.storageService.get('tasks');
+
+    if (tasksSaved) {
+      this.tasks.set(tasksSaved);
+    }
+  }
+
+  public addTask(task: Task) {    
+    task.id = this.makeID();
+
+    this.tasks.update((tasks) => [ task, ...tasks ]);
+    this.saveTasks();
+  }
+
+  public removeTask(taskId: string) {
+    this.tasks.update((tasks) => {
+      return tasks.filter((task) => task.id !== taskId)
+    });
+
+    this.saveTasks();
+  }
+
+  public checkTask(taskId: string, status: boolean) {
+    this.tasks.update((tasks) => {
+      tasks.forEach((task) => {
+        if (task.id === taskId) {
+          task.check = status;
+        }
+      });
+
+      return tasks;
+    });
+
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    this.storageService.save('tasks', this.tasks());
+  }
+
+  private makeID(): string {
+    return Date.now().toString(36) + Math.random().toString(36).substr(2, 5);
+  }
 }
