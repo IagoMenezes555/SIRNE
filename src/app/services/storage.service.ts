48ef62fca5751b4af955119dea3c15f475f3d39c
Érick da silva <eierickdasilva@gmail.com>
2025-04-21 14:23:57 -0300

    feat: adding storage service

---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+  public save(key: string, value: any) {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  public get(key: string): any {
+    const saved = localStorage.getItem(key);
+
+    if (saved) return JSON.parse(saved);
+        
+    return null;
+  }
+
+  public remove(key: string) {
+    localStorage.removeItem(key);
+  }
+}
