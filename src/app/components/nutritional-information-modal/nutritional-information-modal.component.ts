b6247a74652b65e3e997f30502d9aa70abf0f342
Érick da silva <eierickdasilva@gmail.com>
2025-04-21 18:08:21 -0300

    feat: adding nutritional information modal

--- src/app/components/nutritional-information-modal/nutritional-information-modal.component.ts
+++ src/app/components/nutritional-information-modal/nutritional-information-modal.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IonContent } from "@ionic/angular/standalone";
+import { Meal } from 'src/app/models/menu';
 
 @Component({
   selector: 'app-nutritional-information-modal',
   templateUrl: './nutritional-information-modal.component.html',
   styleUrls: ['./nutritional-information-modal.component.scss'],
+  imports: [
+    IonContent,
+  ]
 })
 export class NutritionalInformationModalComponent  implements OnInit {
-
+  @Input() snack: Meal = { calories: 0, carbohydrates: 0, glucose: 0, id: '', lactose: false, name: '', objectId: '', assessment: 0 }
+  @Input() lunch: Meal = { calories: 0, carbohydrates: 0, glucose: 0, id: '', lactose: false, name: '', objectId: '', assessment: 0 }
+  
   constructor() { }
 
   ngOnInit() {}
