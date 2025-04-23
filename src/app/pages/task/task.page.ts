e5b0693042148e4e56da6b088f34fd3a820c6c48
Érick da silva <eierickdasilva@gmail.com>
2025-04-23 05:52:01 -0300

    feat: adding dropdown menu to task list

--- src/app/pages/task/task.page.ts
+++ src/app/pages/task/task.page.ts
@@ -23,6 +23,7 @@ import { Task } from 'src/app/models/task';
 })
 export class TaskPage implements OnInit {
   public tasks = computed(() => this.taskService.tasks());
+  public taskCompletedListOpen: boolean = false;
 
   constructor(
     private taskService: TaskService,
@@ -72,5 +73,9 @@ export class TaskPage implements OnInit {
     });
     
     await alertref.present();
+  }
+
+  public toggleOpenCompletedTask() {
+    this.taskCompletedListOpen = !this.taskCompletedListOpen;
   }
 }
