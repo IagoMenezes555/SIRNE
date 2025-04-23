f2f99a1a66ce6d07345fe7f597e022bdd617f01d
Érick da silva <eierickdasilva@gmail.com>
2025-04-23 19:40:06 -0300

    style: changing icon of blog

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { IonTabs, IonTabBar ,IonTabButton, IonLabel } from '@ionic/angular/standalone';
 
 @Component({
